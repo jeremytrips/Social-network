0384fa2d2c93f080143e52b24724b3c906aa5628
jeremytrips <51384000+jeremytrips@users.noreply.github.com>
2021-04-02 23:50:44 +0200

    File managment

---
+++ src/App.js
@@ -0,0 +1,17 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+import 'react-native-gesture-handler';
+
+import React, {} from 'react';
+import TestNavigator from "./navigators/test_navigator";
+
+const App = () => {
+  return TestNavigator()
+};
+
+export default App;
